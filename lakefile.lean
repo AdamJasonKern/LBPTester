5b2514b8ce154f5ad5a4261f286b4f01fd01f08d
Adam Kern <adamjasonkern@gmail.com>
2025-07-02 10:17:39 -0400

    lakefile.lean update in hopes that leanblueprint finally works

--- lakefile.lean
+++ lakefile.lean
@@ -56,9 +56,3 @@ package Game where
 
 @[default_target]
 lean_lib Game
-
-require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git" @ "4b0563aa6f8b648fc516eb616f54ad47045ae035"
-
-meta if get_config? env = some "dev" then
-require «doc-gen4» from git
-  "https://github.com/leanprover/doc-gen4" @ "main"
